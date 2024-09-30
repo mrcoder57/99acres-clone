d16588719c823446e16951ae28d6e2c9e6a5d882
mrcoder57 <tiwariji2300@gmail.com>
2024-09-30 12:56:42 +0530

    constant for mapping arrays

---
+++ app/utils/constants.tsx
@@ -0,0 +1,35 @@
+export const navLinks = [{
+    name: "For Buyers"
+
+},
+{
+    name: "For Tenants"
+
+},
+{
+    name: "For Owners"
+
+},
+{
+    name: "For Dealers / Builders"
+
+},
+
+{
+    name: "Insights"
+
+},
+]
+export const icons=[
+    {name:"/headphone"},
+    {name:"/people"}
+]
+export const searchFilters=[
+    {name:"Buy"},
+    {name:"Rent"},
+    {name:"New Launch"},
+    {name:"PG/ Co-Living"},
+    {name:"Commercial"},
+    {name:"Plots/Lands"},
+    {name:"Projects"},
+]
