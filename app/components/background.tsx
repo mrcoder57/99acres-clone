6097d10b289ad5c5b49d066fbdd3b884503e31bc
mrcoder57 <tiwariji2300@gmail.com>
2024-09-30 12:59:53 +0530

    make imag responsive

--- app/components/background.tsx
+++ app/components/background.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image';
 
 const Background = () => {
   return (
-    <div className="absolute w-full lg:h-[45vh] h-[50vh] z-0 hover:cursor-pointer overflow-hidden group ">
-      <Image 
-        src="/background.jpg" 
-        alt="background" 
-        layout="fill" 
+    <div className="absolute inset-0 w-full h-[50vh] lg:h-[45vh] z-0 hover:cursor-pointer overflow-hidden group">
+      <Image
+        src="/background.jpg"
+        alt="background"
+        layout="fill"
         objectFit="cover" 
-        className="transition-transform duration-300 ease-in-out transform group-hover:scale-110" 
-        priority 
+        className="transition-transform duration-300 ease-in-out transform group-hover:scale-110"
+        priority
       />
     </div>
   );
-}
+};
 
 export default Background;
