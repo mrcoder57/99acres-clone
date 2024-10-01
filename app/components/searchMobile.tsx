353954ceec1dddd565a3e8dc858513a90ecadb1b
mrcoder57 <tiwariji2300@gmail.com>
2024-10-01 21:17:05 +0530

    fix height of search

--- app/components/searchMobile.tsx
+++ app/components/searchMobile.tsx
@@ -11,11 +11,11 @@ const openSans = Open_Sans({
 
 const Searchmobile = () => {
   return (
-    <div className="absolute lg:hidden  top-[235px] left-[3vh] shadow-xl flex flex-col w-[320px] h-[7vh] rounded-xl bg-white">
- <div className=" flex flex-row items-center mx-3 gap-16 mt-2">
-          <div className=" flex flex-row gap-4">
+    <div className="absolute lg:hidden  top-[235px] left-[3vh] shadow-xl flex flex-col w-[320px] h-16 rounded-xl bg-white">
+ <div className=" flex flex-row items-center justify-center mx-4 gap-16 my-auto">
+          <div className=" flex flex-row items-center justify-center gap-4">
           <Image src="/search.svg" alt="search" width={20} height={20}/>
-          <input type="text" name="" id="" placeholder=" Search Residential " className= {` ${openSans.className} w-[93px] h-10 focus:outline-none text-xs `}/>
+          <input type="text" name="" id="" placeholder=" Search Residential " className= {` ${openSans.className} w-[80px] h-10 focus:outline-none text-xs `}/>
           </div>
           <div className=" flex flex-row gap-2">
           <div className=" h-9 w-9 items-center justify-center bg-blue-100 rounded-full p-2 hover: cursor-pointer">
