764f4470c05081718d5171cfb1c1345257f96a91
mrcoder57 <tiwariji2300@gmail.com>
2024-10-02 20:41:57 +0530

    update mobile bg

--- app/components/mobile-bg.tsx
+++ app/components/mobile-bg.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 const MobileBackground = () => {
   return (
-    <div className="relative lg:hidden flex w-[95%] h-[310px] mt-2 mx-4 overflow-hidden">
+    <div className="relative lg:hidden flex w-[95%] h-[310px] mt-2 mr-3 ml-2 overflow-hidden">
       <div className=" rounded-lg ">
         <Image
           src="/mobileback.jpeg"
